e6bc48ad1dd4f1a873a3f3d3c917fd1d9f896b7c
Karthikeyan Mohan <mkarthik90@gmail.com>
2016-09-21 22:19:51 -0700

    Added a simple solution for finding the maxnnumber from an array

--- Arrays/LargestNumber.java
+++ Arrays/LargestNumber.java
@@ -7,107 +7,56 @@
 */
 
 import java.math.BigInteger;
-public class LargestNumber {
+public class largestNumber {
+    
     // DO NOT MODIFY THE LIST
     
     private int array[];
     private int length;
     
     public String largestNumber(final List<Integer> list) {
-        int[] a = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            a[i] = list.get(i);
-        }
-        
-        sort(a);
-        
-        String result = "";
-        
-        for (int k : array) {
-            result += String.valueOf(k);
-        }
-        
-        while(result.startsWith("0")){
-            result = result.substring(1, result.length());
-        }
-        
-        if(result.isEmpty()) {
-            result = "0";
-        }
-        
-        return result;
-    }
 
-
-    public static int compare(int a, int b) {
-        // 0 Equal
-        // -1 - a > b
-        // 1 - a < b
-        String a1 = String.valueOf(a);
-        String b1 = String.valueOf(b);
-
-        String result1 = a1 + b1;
-        String result2 = b1 + a1;
-
-        BigInteger r1 = new BigInteger(result1);
-        BigInteger r2 = new BigInteger(result2);
-
-        if (r2.compareTo(r1) == -1) {
-            return -1;
-        } else if (r2.compareTo(r1) == 1) {
-            return 1;
+        List<Node> nodes = new ArrayList<>();
+        for (Integer i : list) {
+            Node n = new Node(i);
+            nodes.add(n);
         }
 
-        return 0;
-    }
+        Collections.sort(nodes);
 
-    public void sort(int[] inputArr) {
-         
-        if (inputArr == null || inputArr.length == 0) {
-            return;
-        }
-        this.array = inputArr;
-        length = inputArr.length;
-        quickSort(0, length - 1);
-    }
- 
-    private void quickSort(int lowerIndex, int higherIndex) {
-         
-        int i = lowerIndex;
-        int j = higherIndex;
-        // calculate pivot number, I am taking pivot as middle index number
-        int pivot = array[lowerIndex+(higherIndex-lowerIndex)/2];
-        // Divide into two arrays
-        while (i <= j) {
-            /**
-             * In each iteration, we will identify a number from left side which
-             * is greater then the pivot value, and also we will identify a number
-             * from right side which is less then the pivot value. Once the search
-             * is done, then we exchange both numbers.
-             */
-            while (compare(array[i], pivot) == -1) {
-                i++;
+        StringBuilder builder = new StringBuilder();
+        for (Node node : nodes) {
+            // This has been added in order
+            if(node.val == 0 && builder.length() == 0) {
+                continue;
             }
-            while (compare(array[j], pivot) == 1) {
-                j--;
-            }
-            if (i <= j) {
-                exchangeNumbers(i, j);
-                //move index to next position on both sides
-                i++;
-                j--;
-            }
+            builder.append(node.val);
         }
-        // call quickSort() method recursively
-        if (lowerIndex < j)
-            quickSort(lowerIndex, j);
-        if (i < higherIndex)
-            quickSort(i, higherIndex);
+
+        return builder.toString();
     }
- 
-    private void exchangeNumbers(int i, int j) {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+
+    public class Node implements Comparable<Node> {
+        int val;
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        @Override
+        public int compareTo(Node n) {
+            StringBuffer buffer = new StringBuffer("");
+            buffer.append(n.val);
+            buffer.append(this.val);
+
+            StringBuffer buffer2 = new StringBuffer("");
+            buffer2.append(this.val).append(n.val);
+
+            return buffer.toString().compareTo(buffer2.toString());
+        }
+
     }
+
+
+
 }
